88b7ae75f23493bb366b3d866272383d8f0690e1
naergaga <meishuai99@163.com>
2018-01-26 16:27:58 +0800

    通过关键字查询

--- LocalList/Pages/Projects/Index.cshtml.cs
+++ LocalList/Pages/Projects/Index.cshtml.cs
@@ -23,13 +23,13 @@ namespace LocalList.Pages.Projects
         public IList<Project> Project { get;set; }
         public PageOption PageOption { get; set; }
 
-        public void OnGet(int? current,int? size)
+        public void OnGet(int? current,int? size,string q)
         {
             var po = new PageOption();
             po.Current = current ?? 1;
             po.Size = size ?? 5;
             int skipNum = (po.Current - 1) * po.Size;
-            var query = _context.Project;
+            var query = _context.Project.Where(t=>string.IsNullOrEmpty(q) || t.Name.Contains(q) || t.Title.Contains(q) || t.Description.Contains(q));
 
             po.Count = query.Count();
             Project = query.Skip(skipNum).Take(po.Size).ToList();
