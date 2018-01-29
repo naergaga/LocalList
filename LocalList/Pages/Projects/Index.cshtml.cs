b725c3402b7559b560aa23edbd0e5521a5050512
naergaga <meishuai99@163.com>
2018-01-29 22:57:54 +0800

    修复标签的linq查询

--- LocalList/Pages/Projects/Index.cshtml.cs
+++ LocalList/Pages/Projects/Index.cshtml.cs
@@ -8,6 +8,7 @@ using Microsoft.EntityFrameworkCore;
 using LocalList.Data;
 using LocalList.Data.Model;
 using LocalList.Utilities.Model;
+using System.Collections;
 
 namespace LocalList.Pages.Projects
 {
@@ -32,15 +33,18 @@ namespace LocalList.Pages.Projects
             po.Current = current ?? 1;
             po.Size = size ?? 5;
             int skipNum = (po.Current - 1) * po.Size;
-            var query = from p in _context.Project
-                        join pt in _context.ProjectTag on p.Id equals pt.ProjectId into gj
-                        from pt in gj.DefaultIfEmpty()
-                        join t in _context.Tag on pt.TagId equals t.Id into gj2
-                        from t in gj2.DefaultIfEmpty()
-                        where (string.IsNullOrEmpty(q) || (p.Name.Contains(q) || p.Title.Contains(q) || p.Description.Contains(q)))
-                        && (tags.Count == 0 || tags.Contains(t.Id))
-                        select p;
+            //tag Id 对应的 project Id
+            var projectId = from t in _context.Tag
+                         join pt in _context.ProjectTag on t.Id equals pt.TagId
+                         where tags.Contains(t.Id)
+                         select pt.ProjectId;
 
+            var query = from p in _context.Project
+                        //是否包含关键字
+                         where (string.IsNullOrEmpty(q) || (p.Name.Contains(q) || p.Title.Contains(q) || p.Description.Contains(q)))
+                         //不查询标签或 项目Id为
+                         && (tags.Count == 0 || projectId.Contains(p.Id))
+                         select p;
             po.Count = query.Count();
             Project = query.Skip(skipNum).Take(po.Size).ToList();
             PageOption = po;
